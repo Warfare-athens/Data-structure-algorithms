e2a3ef724ba69b07094e145711eaeb2973266666
Athens Dubey <136609989+Warfare-athens@users.noreply.github.com>
2024-08-28 22:29:22 +0530

    Create LeetCode 225 - Implement Stack using Queues.py

---
+++ LeetCode 225 - Implement Stack using Queues.py
@@ -0,0 +1,18 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        for i in range(0,len(self.q)-1):
+            self.q.append( self.q.popleft() )
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[-1]
+        
+    def empty(self) -> bool:
+        return len(self.q) == 0
