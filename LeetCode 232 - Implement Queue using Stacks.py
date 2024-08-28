8f012a03399698e9c196c316cb911e686ba8a850
Athens Dubey <136609989+Warfare-athens@users.noreply.github.com>
2024-08-28 23:07:08 +0530

    Create LeetCode 232 - Implement Queue using Stacks.py

---
+++ LeetCode 232 - Implement Queue using Stacks.py
@@ -0,0 +1,23 @@
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
+                
+    def empty(self) -> bool:
+        return len(self.s1) == 0 and len(self.s2) == 0 
