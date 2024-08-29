9156ef0914414aea8df6d9fba3014fb621a618c0
Athens Dubey <136609989+Warfare-athens@users.noreply.github.com>
2024-08-29 22:19:07 +0530

    Create LeetCode 155 - Min Stack.py

---
+++ LeetCode 155 - Min Stack.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.min_stack:
+            self.min_stack.append(min(val, self.min_stack[-1]))
+        else:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1] 
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
